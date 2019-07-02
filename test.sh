afe58f82a3d2c31bceb87d925f1cc3fb92993903
amod.kadam@cloudmanthan.com <amod.kadam@cloudmanthan.com>
2019-07-02 20:06:50 +0530

    run6

--- test.sh
+++ test.sh
@@ -3,9 +3,6 @@ echo "running addition script"
 # SECONDNUMBER=20
 NUM=$(($FIRSTNUMBER+$SECONDNUMBER))
 echo $NUM
-<<<<<<< HEAD
 echo "not working"
-=======
 echo "running through jenkins integration-5"
->>>>>>> df485d7fb7974a8513b70be10bb738c7d865445b
 
